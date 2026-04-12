804fb875995a59837b5b2b3e13fc0b1ccc18016d
Gontu rama krishna reddy <ramakrishna@Gontus-MacBook-Air.local>
2026-04-12 09:40:43 +0530

    st ch

--- app.py
+++ app.py
@@ -309,12 +309,12 @@ if st.button("🚀 Generate AI Poster"):
     
     
 
-    <div style="display:flex;justify-content:center;align-item:center">
+    <div style="display:flex;justify-content:center;align-items:center;gap:10px">
 
         <img src="{phone_icon}" style="width:35px;height:35px;">
         <p style="font-size:45px;">{customer_phone}</p>
     </div>    
-    <div style="display:flex;justify-content:center;align-item:center">
+    <div style="display:flex;justify-content:center;align-items:center;gap:10px">
         <img src="{location_icon}" style="width:35px;height:35px;">
         <p style="font-size:45px;">{customer_address}</p>
        
