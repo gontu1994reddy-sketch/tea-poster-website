f4d26ed9d6a4eca7b3f5d1db6c0381084beedfeb
Gontu rama krishna reddy <ramakrishna@Gontus-MacBook-Air.local>
2026-04-14 13:49:27 +0530

    ghdj

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from streamlit_gsheets import GSheetsConnection
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 try:
-    data = conn.read(ttl=300)
+    data = conn.read(ttl=500)
     st.success("✅ Connected")
     st.write(data)
     st.dataframe(data)
