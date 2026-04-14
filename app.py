b84534e789cebc6d8b5120994a8818870fcff692
Gontu rama krishna reddy <ramakrishna@Gontus-MacBook-Air.local>
2026-04-14 21:04:59 +0530

    err fi

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ if customer_phone.strip() and customer_phone != st.session_state.last_phone:
                 "auth_uri" : "https://accounts.google.com/o/oauth2/auth",
                 "token_uri" : "https://oauth2.googleapis.com/token",
                 "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-                "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/{st.secrets["connections"]["gsheets"]["client_email"]}"
+                "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/{st.secrets['connections']['gsheets']['client_email']}"
             }
             
     
