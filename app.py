d67392dc7d447bd034c4958870e0d6814c514a39
Gontu rama krishna reddy <ramakrishna@Gontus-MacBook-Air.local>
2026-04-14 13:47:36 +0530

    ficcc

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from streamlit_gsheets import GSheetsConnection
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 try:
-    data = conn.read(ttl=60)
+    data = conn.read(ttl=300)
     st.success("✅ Connected")
     st.write(data)
-    st.write(type(data))
+    st.dataframe(data)
 except Exception as e:
     st.error(f"Google sheet error: {e}")
